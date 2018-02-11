566f5f2199fc75976c1a2ab8d22bbafd4d3a6aad
Dejalin <dejalin.pradhan@primefocus.com>
2018-02-11 14:58:20 +0530

    first change

--- DevopsDemo/Home.aspx
+++ DevopsDemo/Home.aspx
@@ -10,6 +10,9 @@
     <form id="form1" runat="server">
     <div>
      <b><asp:Label ID="lblName" runat="server" Text="Welcome to Devops!" style="font-size:medium; font-style:italic"></asp:Label></b>
+        <asp:Button ID="btnAdd" runat="server" Text="Add"/>
+        <asp:Label ID="lblmsg" runat="server" Text="Hi"></asp:Label>
+        
     </div>
     </form>
 </body>
--- DevopsDemo/Home.aspx.designer.cs
+++ DevopsDemo/Home.aspx.designer.cs
@@ -29,5 +29,23 @@ namespace DevopsDemo {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblName;
+        
+        /// <summary>
+        /// btnAdd control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAdd;
+        
+        /// <summary>
+        /// lblmsg control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblmsg;
     }
 }
